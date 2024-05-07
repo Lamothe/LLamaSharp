addc53926ed4b0726b71467e0dffd1428d1746e9
Michael Lamothe <michael.lamothe@gmail.com>
2024-05-07 22:53:07 +1000

    Improved UI

--- LLama.Web/Pages/Index.cshtml.cs
+++ LLama.Web/Pages/Index.cshtml.cs
@@ -3,39 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
 
-namespace LLama.Web.Pages
+namespace LLama.Web.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger, IOptions<LLamaOptions> options)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+        Options = options.Value;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger, IOptions<LLamaOptions> options)
+    public LLamaOptions Options { get; set; }
+
+    [BindProperty]
+    public ISessionConfig SessionConfig { get; set; }
+
+    [BindProperty]
+    public InferenceOptions InferenceOptions { get; set; }
+
+    public void OnGet()
+    {
+        SessionConfig = new SessionConfig
         {
-            _logger = logger;
-            Options = options.Value;
-        }
+            Prompt = "Below is an instruction that describes a task. Write a response that appropriately completes the request.",
+            AntiPrompt = "User:",
+            OutputFilter = "User:, Assistant:, A:, Response:"
+        };
 
-        public LLamaOptions Options { get; set; }
-
-        [BindProperty]
-        public ISessionConfig SessionConfig { get; set; }
-
-        [BindProperty]
-        public InferenceOptions InferenceOptions { get; set; }
-
-        public void OnGet()
+        InferenceOptions = new InferenceOptions
         {
-            SessionConfig = new SessionConfig
-            {
-                Prompt = "Below is an instruction that describes a task. Write a response that appropriately completes the request.",
-                AntiPrompt = "User:",
-                OutputFilter = "User:, Assistant: "
-            };
-
-            InferenceOptions = new InferenceOptions
-            {
-                Temperature = 0.8f
-            };
-        }
+            Temperature = 0.8f
+        };
     }
 }
